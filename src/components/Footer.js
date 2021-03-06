b50b322d522d5944e0b35fc8a8ae9d5d954664c8
programmia tech <programmiatech@gmail.com>
2021-03-06 15:11:47 +0000

    Footer.js: updated div

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-black small text-center text-white-50">
       <div className="container">
-        Copyright &copy; Alright Re
+        Copyright &copy; Alrights Reserved
       </div>
     </footer>
   );
